1044f695e7b49b5831f7e8d94896b065a8bd28cb
vijaybaliah <vijaybalu1989@gmail.com>
2019-02-24 12:50:37 +0530

    feat: added webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 }
 
-module.exports = {
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+      },
+      historyApiFallback: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      ...commonConfig.plugins,
+      new webpack.WatchIgnorePlugin([
+        path.join(__dirname, 'node_modules')
+      ])
+    ],
+  }
+
+  return { ...commonConfig, ...config }
+}
+
+const commonConfig = {
   entry: {
     app: PATHS.app,
   },
@@ -19,4 +45,12 @@ module.exports = {
       title: 'Webpack demo'
     })
   ]
+}
+
+module.exports = (env) => {
+  if (env === 'production') {
+    return productionConfig();
+  }
+
+  return developmentConfig();
 }
